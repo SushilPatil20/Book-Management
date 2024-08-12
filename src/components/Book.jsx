8f054369cea55bd4e03d13ab735927992420ea41
PatilSushil20 <124390002+PatilSushil20@users.noreply.github.com>
2024-08-12 09:04:13 +0530

    Added Search functionality and with that simple Book Management application is finished...

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
-const Book = () => {
+const Book = ({ book }) => {
   return (
     <>
-      <img src="" alt="" />
-      <div>
-        <h2></h2>
-      </div>
+      <li
+        className=" hover:scale-105 duration-200 min-w-fit m-4 bg-white rounded-lg shadow-md
+       shadow-gray-700 overflow-hidden mb-4"
+      >
+        <div className="px-6 py-4">
+          <h2 className="text-xl font-bold text-gray-900 mb-2">{book.title}</h2>
+          <p className="text-gray-700 text-base mb-4">{book.description}</p>
+          <div className="flex items-center">
+            <img
+              className="w-10 h-10 rounded-full mr-4"
+              src={book.coverImage}
+              alt={book.title}
+            />
+            <div className="text-sm">
+              <p className="text-gray-900 leading-none">{book.author}</p>
+              <p className="text-gray-600">{book.publishedDate}</p>
+            </div>
+          </div>
+        </div>
+      </li>
     </>
   );
 };
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,5 +1,19 @@
-const BookList = (props) => {
-  return <></>;
+import Book from "./Book";
+
+const BookList = ({ books }) => {
+  return (
+    <>
+      <ul className="grid grid-cols-1 md:grid-cols-3 space-4 px-6">
+        {books.length !== 0 ? (
+          books.map((book) => <Book key={book.id} book={book} />)
+        ) : (
+          <h2 className="text-center text-2xl text-gray-700">
+            Search not found..!!
+          </h2>
+        )}
+      </ul>
+    </>
+  );
 };
 
 export default BookList;
